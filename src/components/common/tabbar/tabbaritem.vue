4991e3ecb637f97d516561efbaaf1d896923c94d
panderstar <2215437227@qq.com>
2021-06-24 17:35:50 +0800

    架子搭好

---
+++ src/components/common/tabbar/tabbaritem.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- <div> -->
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+    <!-- <slot name="item-text"></slot> -->
+  </div>
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // isActive: false,
+    };
+  },
+  props: {
+    path: String,
+    itemColor: {
+      type: String,
+      default: "rgba(77, 220, 245, 0.925)",
+    },
+  },
+  methods: {
+    itemClick() {
+      // console.log(this.$router);
+      this.$router.replace(this.path);
+    },
+  },
+  computed: {
+    isActive() {
+      return this.$route.path == this.path;
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.itemColor } : {};
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+  /* line-height: 49px; */
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+/* .active {
+  color: itemColor;
+} */
+</style>
---
+++ src/components/content/mainTabbar/mainTabbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <Tabbar>
+    <Tabbaritem path="/home">
+      <!-- css样式里引入别名要在前面加~ -->
+      <img slot="item-icon" src="~assets/img/tabbar/home.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/home_active.png"
+        alt=""
+      />
+      <div slot="item-text">首页</div>
+    </Tabbaritem>
+    <Tabbaritem path="/category">
+      <img slot="item-icon" src="~assets/img/tabbar/category.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/category_active.png"
+        alt=""
+      />
+      <div slot="item-text">分类</div>
+    </Tabbaritem>
+    <Tabbaritem path="/cart">
+      <img slot="item-icon" src="~assets/img/tabbar/shopcart.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/shopcart_active.png"
+        alt=""
+      />
+      <div slot="item-text">购物车</div>
+    </Tabbaritem>
+    <Tabbaritem path="/profile">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.png" alt="" />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/profile_active.png"
+        alt=""
+      />
+      <div slot="item-text">我的</div>
+    </Tabbaritem>
+  </Tabbar>
+</template>
+
+<script>
+import Tabbar from "components/common/tabbar/tabbar.vue";
+import Tabbaritem from "components/common/tabbar/tabbaritem.vue";
+export default {
+  components: {
+    Tabbar,
+    Tabbaritem,
+  },
+};
+</script>
+
+<style>
+</style>
